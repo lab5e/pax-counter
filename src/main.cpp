1a4e21061b5fc512dbca9fa2c4ad8e68417fa6e7
Hans Jørgen Grimstad <hansj@lab5e.com>
2021-07-06 13:07:18 +0200

    Added time triggers for TX and reboot

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@
 
 MACAddressPool pool(300000); // Age limit: 5 minutes
 
+#define TRANSMIT_DELAY_MS 1000 * 60 * 5 // Transmit every 5 minutes
+#define BOOT_DELAY_MS 1000 * 60 * 60  // Reboot every hour
+
+void(* resetFunc) (void) = 0;
+
 void setup() 
 {
   wifi_scanner_setup();
@@ -17,13 +22,32 @@ void setup()
 
 void loop() 
 {
+  unsigned long boot_time_start, boot_time_end, transmit_time_start, transmit_time_end;
+
+  boot_time_start = millis();
   while (true)
   {
-    wifi_scan();
-    ble_scan();
-    nbiot_status();
-    nbiot_transmit_message(pool.get_count(BT), pool.get_count(WIFI));
-    pool.Purge();
-    pool.Log();
+    transmit_time_start = millis();
+    while (true)
+    {
+      wifi_scan();
+      ble_scan();
+      pool.Log();
+      transmit_time_end  = millis();
+      boot_time_end = millis();
+      if (transmit_time_end-transmit_time_start > TRANSMIT_DELAY_MS)
+      {
+        Serial.println("----- Transmitting -----");
+        nbiot_status();
+        nbiot_transmit_message(pool.get_count(BT), pool.get_count(WIFI));
+        pool.Purge();
+        Serial.println("----- Log purged -----");
+        break;
+      }
+    }
+    if (boot_time_end-boot_time_start > BOOT_DELAY_MS)
+    {
+      resetFunc();
+    }
   }
 }
